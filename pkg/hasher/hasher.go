e3ee3e870b44fb566560cb016eeeecbe81db8f3a
OTereshchenko <O.Tereshchenko@wolterskluwer.com>
2023-04-21 14:10:15 +0300

    remove error handling from hasher.go

--- pkg/hasher/hasher.go
+++ pkg/hasher/hasher.go
@@ -2,16 +2,12 @@ package hasher
 
 import (
 	"golang.org/x/crypto/bcrypt"
-	"log"
 )
 
 const cost = 10
 
 func HashPassword(password string) (string, error) {
 	var bytes, err = bcrypt.GenerateFromPassword([]byte(password), cost)
-	if err != nil {
-		log.Fatal("Server error while password hash")
-	}
 
 	return string(bytes), err
 }
